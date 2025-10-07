75212cb236061fc8e3f56cf0d34083a89fd8ca2b
TiagoVazLopes95 <manuelvazlopes13>
2025-10-07 03:33:37 +0100

    Add PropertiesController

---
+++ OmahRealEstate.Web/Controllers/PropertiesController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OmahRealEstate.Web.Data;
+using OmahRealEstate.Web.Data.Entities;
+using OmahRealEstate.Web.Data.Repositories.Interfaces;
+
+namespace OmahRealEstate.Web.Controllers
+{
+    public class PropertiesController : Controller
+    {
+        private readonly IPropertyRepository _propertyRepository;
+
+        public PropertiesController(IPropertyRepository propertyRepository)
+        {
+            _propertyRepository = propertyRepository;
+        }
+
+        // GET: Properties
+        public IActionResult Index()
+        {
+            return View(_propertyRepository.GetAll());
+        }
+
+        // GET: Properties/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var property = await _propertyRepository.GetByIdAsync(id.Value);
+
+            if (property == null)
+            {
+                return NotFound();
+            }
+
+            return View(property);
+        }
+
+        // GET: Properties/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Properties/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Property property)
+        {
+            if (ModelState.IsValid)
+            {
+                await _propertyRepository.CreateAsync(property);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(property);
+        }
+
+        // GET: Properties/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var property = await _propertyRepository.GetByIdAsync(id.Value);
+
+            if (property == null)
+            {
+                return NotFound();
+            }
+
+            return View(property);
+        }
+
+        // POST: Properties/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Property property)
+        {
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _propertyRepository.UpdateAsync(property);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!await _propertyRepository.ExistAsync(property.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@property);
+        }
+
+        // GET: Properties/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var property = await _propertyRepository.GetByIdAsync(id.Value);
+
+            if (property == null)
+            {
+                return NotFound();
+            }
+
+            return View(property);
+        }
+
+        // POST: Properties/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var property = await _propertyRepository.GetByIdAsync(id);
+            await _propertyRepository.DeleteAsync(property);   
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
